5042394358bb0d1619e954a7c171703929282660
Yinghao Zhu <zhuyinghao1999@msn.cn>
2020-11-17 08:39:37 +0800

    GORM create table succeed
    Former-commit-id: a1b7435bfb3041c1c4aa6734a28f9c2988f82871

--- Go+Gin+GORM/gin-project/src/main.go
+++ Go+Gin+GORM/gin-project/src/main.go
@@ -3,12 +3,21 @@ package main
 import (
 	"fmt"
 	"gin-project/src/dao"
+	"time"
 
 	"github.com/gin-gonic/gin"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
+
+type User struct {
+	gorm.Model
+	Birthday time.Time
+	Age      int
+	Name     string `gorm:"size:255"`       // string默认长度为255, 使用这种tag重设。
+	Num      int    `gorm:"AUTO_INCREMENT"` // 自增
+}
 
 func main() {
 	db, err := gorm.Open("mysql",
@@ -20,6 +29,18 @@ func main() {
 	} else {
 		fmt.Println("connection succeeded")
 	}
+
+	// 创建表`users'时将“ENGINE = InnoDB”附加到SQL语句
+	db.Set("gorm:table_options", "ENGINE=InnoDB").CreateTable(&User{})
+
+	// 检查模型`User`表是否存在
+	fmt.Println(db.HasTable(&User{}))
+
+	// 检查表`users`是否存在
+	fmt.Println(db.HasTable("users"))
+
+	fmt.Println("--------------------------------")
+
 	defer db.Close()
 
 	dao.New()
