368e3664ae463be2c3372e3a91591659b1b4e705
Yinghao Zhu <zhuyinghao1999@msn.cn>
2021-08-27 23:57:45 +0800

    feat: wangeditor vue project

---
+++ EnvSettings/vue2-utils/Editor.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="editor-root">
+      <div id="editor" style="text-align: left"></div>
+      <div v-html="content"></div>
+      <button id="save" @click="save_content">保存</button>
+    </div>
+  </template>
+  
+  <script>
+  import E from "wangeditor"
+  export default {
+    data(){
+      return {
+        content: "",
+        editor: null
+      }
+    },
+    mounted() {
+      var _this = this
+      var editor = new E('#editor');
+      _this.editor = editor
+      console.log(editor);
+      _this.editor.config.uploadImgShowBase64 = true
+      _this.editor.create();
+    },
+    methods: {
+      save_content() {
+        var _this=this;
+        console.log(_this.content)
+        _this.content=_this.editor.txt.html()
+        console.log(_this.content)
+      }
+    }
+    
+  };
+  </script>
